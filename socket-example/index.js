f3b3b6153e374b87041b1e0c845cba31a21d8405
Na Yoon Kang <nykang17@gmail.com>
2024-10-26 22:29:29 -0700

    Adding more functionality to lobby
    I added some more functionality to our lobby system. Players will now see a list
    of all the current users in the same room. When a player leaves, this will
    be reflected on that list. Players can add a name for themselves, and information
    of these players are saved into an array. Added comments explaining committed

--- socket-example/index.js
+++ socket-example/index.js
@@ -1,5 +1,3 @@
-// need express, socketio, cors (for react), and nodemon
-
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -11,6 +9,31 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"))
+
+// state 
+// each user has name, ID, room ID associated with it, ADD A FIELD INDICATING PLAYER # USING ROOM SIZE
+const UsersState = {
+    users: [],
+    setUsers: function (newUsersArray) {
+        this.users = newUsersArray
+    }
+}
+
+// CREATE A ROOM STATE THAT KEEPS TRACK OF EXISTING ROOMS (THIS MIGHT NOT BE NEEDED)
+// example room: 
+/*
+room = {
+    id: lemon
+    size: #
+    players: (list)
+}
+*/
+const RoomState = {
+    rooms: [],
+    setRooms: function (newRoomArray) {
+        this.rooms = newRoomArray
+    }
+}
 
 const io = new Server(server, {
     cors: {
@@ -24,18 +47,73 @@ io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`)
 
     // server practically puts the client into a separate "room" by an identifier that we give it
-    socket.on("join_room", (data) => {
-        socket.join(data.room);
+    socket.on("join_room", ({room, name}) => {
+
+        // create a user object once they've made a name and joined a room, store it
+        const user = activateUser(socket.id, name, room)
+
+        socket.join(user.room);
+
         // socket.leave is a way to manually make a client leave a room
         // would be useful for allowing people to join other rooms (so they can leave the previous one)
+
+        // Update user list for room when someone joins
+        io.to(user.room).emit('userList', {
+            users: getUsersInRoom(user.room)
+        })
+
+        // TO SEND A MESSAGE PRIVATELY TO ONE SPECIFIC USER:
+        // io.to(socketId).emit('privateMessage', 'Hello, this is a private message');
+        // WE CAN USE THIS WHEN CERTAIN PLAYERS ARE COMPOSING, AND THE REST ARE WAITING
     });
 
-    // server receives the message sent by client and sends it back to ALL clients
+    // handler for when client sends a chat message to the server
     socket.on("send_message", (data) => {
+
+        // server receives the message sent by client and sends it back to ALL clients
         io.to(data.room).emit("receive_message", `${socket.id.substring(0,5)}: ${data.msg}`)
     });
+
+    // handler for when a player disconnects
+    socket.on('disconnect', () => {
+        const user = getUser(socket.id)
+        userLeavesApp(socket.id)
+
+        // update the user list that players will see in their rooms
+        if (user) {
+            io.to(user.room).emit('userList', {
+                users: getUsersInRoom(user.room)
+            })
+        }
+        console.log(`User ${socket.id} disconnected`)
+    })
 })
 
 server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
+
+
+// User functions 
+function activateUser(id, name, room) {
+    const user = { id, name, room }
+    UsersState.setUsers([
+        ...UsersState.users.filter(user => user.id !== id),
+        user
+    ])
+    return user
+}
+
+function userLeavesApp(id) {
+    UsersState.setUsers(
+        UsersState.users.filter(user => user.id !== id)
+    )
+}
+
+function getUser(id) {
+    return UsersState.users.find(user => user.id === id)
+}
+
+function getUsersInRoom(room) {
+    return UsersState.users.filter(user => user.room === room)
+}
--- socket-example/public/app.js
+++ socket-example/public/app.js
@@ -1,8 +1,10 @@
 const socket = io.connect('http://localhost:3000');
 
-// define the variables for chatroom
+// define the variables for chatroom (THESE WOULD PROBABLY BE STATES IN REACT)
 const msg = document.querySelector('#message')
+const nameInput = document.querySelector('#name')
 const chatRoom = document.querySelector('#room')
+const usersList = document.querySelector('.user-list')
 
 // WE CAN JUST HAVE A DATA STRUCTURE THAT STORES EACH ROOM ID IF NEEDED
 
@@ -22,8 +24,11 @@ const sendMessage = (e) => {
 // once clients input a room ID and presses join, a request is sent to the server to let them join that room
 const joinRoom = (e) => {
     e.preventDefault()
-    if(chatRoom.value) {
-        socket.emit("join_room", { room: chatRoom.value })
+    if(chatRoom.value && nameInput.value) {
+        socket.emit("join_room", { 
+            room: chatRoom.value,
+            name: nameInput.value 
+        })
     }
 };
 
@@ -34,8 +39,26 @@ socket.on("receive_message", (data) => {
     document.querySelector('ul').appendChild(li)
 })
 
+// handler for when server sends all users in current room
+socket.on('userList', ({ users }) => {
+    showUsers(users)
+})
+
 document.querySelector('.form-msg')
     .addEventListener('submit', sendMessage)
 
 document.querySelector('.form-join')
     .addEventListener('submit', joinRoom)
+
+function showUsers(users) {
+    usersList.textContent = ''
+    if (users) {
+        usersList.innerHTML = `<em>Users in ${chatRoom.value}:</em>`
+        users.forEach((user, i) => {
+            usersList.textContent += ` ${user.name}`
+            if (users.length > 1 && i !== users.length - 1) {
+                usersList.textContent += ","
+            }
+        })
+    }  
+}
