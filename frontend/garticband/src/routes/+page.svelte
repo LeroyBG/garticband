bd77410df5ade310bd496eb3fb47210425f63530
Parikshit Sood <parkysood@gmail.com>
2024-11-14 13:44:01 -0800

    changed UI of home page, buttons and logo now side-by-side

--- frontend/garticband/src/routes/+page.svelte
+++ frontend/garticband/src/routes/+page.svelte
@@ -1,49 +1,66 @@
-<div class="h-full flex flex-col justify-center items-center bg-gradient-to-b from-purple to-pink">
-    <div class="text-purple-800 flex flex-col items-center justify-center gap-3">
-        <img src={logo} alt="logo"/>
-        <div><a href={createGamePageURL}>create game</a></div>
-        <div>
-            {#if !validGameId}
-            join game --&gt; 
-            {:else}
-            <a href={joinGamePageURL}
-                class="bg-purple-600 text-white border-r-1 rounded-md p-1"
-                >join game
-            </a> &lt--
-            {/if}
-            <input placeholder="game id" 
-                class="placeholder:text-center w-20 text-green-500 text-center"
-                bind:value={gameIdInput}
-                />
-        </div>
-    </div>
-</div>
-
 <script lang="ts">
-    import logo from "$lib/images/garticband.png"
-    let gameIdInput = $state<string>("");
-    const gamePageURL = "/game"
-    let createGamePageURL = $state("")
+  import logo from "$lib/images/garticband.png";
+  let gameIdInput = $state<string>("");
+  let playerName = $state<string>("");
+  const gamePageURL = "/game";
+  let createGamePageURL = $state("");
 
-    // TODO: get this to run at server with node:crypto module
-    $effect(() => {
-        createGamePageURL = gamePageURL + '?' + new URLSearchParams({
-            "id": crypto.randomUUID()
-        }).toString()
-    })
+  $effect(() => {
+    createGamePageURL =
+      gamePageURL +
+      "?" +
+      new URLSearchParams({
+        id: crypto.randomUUID(),
+      }).toString();
+  });
 
-    const joinGamePageURL = $derived(gamePageURL + '?' + new URLSearchParams({
-        "id": gameIdInput
-    }))
+  const joinGamePageURL = $derived(
+    gamePageURL +
+      "?" +
+      new URLSearchParams({
+        id: gameIdInput,
+      })
+  );
 
-    const validateGameId = (id: string): boolean => {
-        // TODO: Actually validate gameId
-        return true
-    }
+  const validateGameId = (id: string): boolean => {
+    return true;
+  };
 
-    let validGameId = $derived(validateGameId(gameIdInput))
+  let validGameId = $derived(validateGameId(gameIdInput));
 </script>
 
-<style>
-    
-</style>
+<div class="h-full flex bg-gradient-to-b from-purple to-pink">
+  <div class="flex-1 flex justify-center items-center">
+    <img src={logo} alt="logo" class="w-1/2" />
+  </div>
+  <div
+    class="flex-1 flex flex-col items-center justify-center gap-4 text-center"
+  >
+    <input
+      placeholder="Enter Name"
+      class="placeholder:text-center w-80 text-green-500 text-center border p-2 rounded-md"
+      bind:value={playerName}
+    />
+    <input
+      placeholder="Enter Room ID"
+      class="placeholder:text-center w-80 text-green-500 text-center border p-2 rounded-md"
+      bind:value={gameIdInput}
+    />
+
+    <div class="bg-beige p-2 rounded-md text-xl w-40">
+      <a href={createGamePageURL}>Create Game</a>
+    </div>
+    <div class="bg-lightred p-2 rounded-md text-xl w-40">
+      {#if !validGameId}
+        join game --&gt;
+      {:else}
+        <a
+          href={joinGamePageURL}
+          class="bg-purple-600 text-white rounded-md p-1"
+        >
+          Join Game
+        </a>
+      {/if}
+    </div>
+  </div>
+</div>
