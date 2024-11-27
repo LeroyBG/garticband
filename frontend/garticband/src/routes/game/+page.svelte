0beb872a17656cd4044a50c0c0a4f51a0630bdeb
Na Yoon Kang <nykang17@gmail.com>
2024-11-26 16:28:25 -0800

    Changing structure of lobby page
    just switched around the script and html section

--- frontend/garticband/src/routes/game/+page.svelte
+++ frontend/garticband/src/routes/game/+page.svelte
@@ -1,188 +1,3 @@
-<script lang="ts">
-  import { page } from "$app/stores";
-  import { io } from "$lib/socketConnection";
-  import logo from "$lib/images/garticband.png";
-  import playerIcon from "$lib/images/artist.png";
-  import { browser } from "$app/environment";
-
-  // Import components
-  import UpNext from "$lib/components/UpNext.svelte";
-  import AlreadyWent from "$lib/components/AlreadyWent.svelte";
-  import WaitingForPlayers from "$lib/components/WaitingForPlayers.svelte";
-  import FinalComposition from "$lib/components/FinalComposition.svelte";
-
-  // Types
-  import { type playerInRoom, type roomInfo } from "$lib/types";
-  import SelectInstrument from "$lib/components/SelectInstrument.svelte";
-
-  // Name of user
-  let username = "";
-  if (browser) {
-    username = localStorage.getItem("username") || "No Name";
-  }
-
-  const URLParams = $page.url.searchParams;
-  let roomId = $state<string | null>(null);
-  let roomState = $state<roomInfo>({
-    players: [],
-    selectPhase: false,
-    activeTurn: null,
-    isCompleted: false,
-  });
-
-  let takenInstruments = $state({
-    drums: false,
-    piano: false,
-    synth: false,
-    bass: false,
-  });
-
-  let genre = $state<string | null>("");
-
-  const NUM_TIMESTEPS = 32;
-
-  // Find ourself in the array
-  let me = $derived<playerInRoom | null>(
-    roomState.players.find((p) => p.id == io.id) ?? null
-  );
-
-  let instrumentSelectActive = $derived<boolean>(roomState.selectPhase == true);
-  let instrumentDone = $derived<boolean>(
-    Object.values(takenInstruments).every((value) => value === true)
-  );
-
-  let roomReadyToStart = $derived<boolean>(
-    !roomState.activeTurn && roomState.players.length == 4
-  );
-  let gameActive = $derived<boolean>(roomState.activeTurn != null);
-  let gameFinished = $derived<boolean>(roomState.isCompleted);
-
-  let upNext = $derived<boolean | null>(
-    roomState.activeTurn && me?.turnNumber
-      ? roomState.activeTurn == me.turnNumber - 1
-      : null
-  );
-
-  let activeTurn = $derived<boolean | null>(
-    roomState.activeTurn && me?.turnNumber
-      ? roomState.activeTurn == me.turnNumber
-      : null
-  );
-
-  let alreadyWent = $derived<boolean | null>(
-    roomState.activeTurn && me?.turnNumber
-      ? roomState.activeTurn > me.turnNumber
-      : null
-  );
-
-  const startSelect = () => {
-    io.emit("start_select", {});
-  };
-
-  const chooseDrum = () => {
-    // add a check where it only does this if we don't have an instrument selected
-    if (
-      me?.sequencer.instrumentId == null &&
-      takenInstruments["drums"] == false
-    ) {
-      io.emit("choose_drum", {});
-    }
-  };
-  // add other instrument selections
-  const chooseSynth = () => {
-    // add a check where it only does this if we don't have an instrument selected
-    if (
-      me?.sequencer.instrumentId == null &&
-      takenInstruments["synth"] == false
-    ) {
-      io.emit("choose_synth", {});
-    }
-  };
-
-  const chooseBass = () => {
-    // add a check where it only does this if we don't have an instrument selected
-    if (
-      me?.sequencer.instrumentId == null &&
-      takenInstruments["bass"] == false
-    ) {
-      io.emit("choose_bass", {});
-    }
-  };
-
-  const choosePiano = () => {
-    // add a check where it only does this if we don't have an instrument selected
-    if (
-      me?.sequencer.instrumentId == null &&
-      takenInstruments["piano"] == false
-    ) {
-      io.emit("choose_piano", {});
-    }
-  };
-
-  const startGame = () => {
-    io.emit("start_game", {});
-  };
-  $effect(() => {
-    if (!URLParams.get("id")) {
-      // Something went wrong...
-      return;
-    }
-    const id = URLParams.get("id");
-    io.emit("join_room", { roomId: id, name: username });
-
-    // * * * Setup socket stuff
-    io.on("room_joined", (data) => {
-      roomId = data.roomId;
-      roomState = data.roomState;
-    });
-
-    io.on("player_joined", (data) => {
-      roomState = data.roomState;
-    });
-
-    io.on("player_left", (data) => {
-      roomState = data.roomState;
-    });
-
-    io.on("select_started", (data) => {
-      roomState = data.roomState;
-      genre = data.genre;
-    });
-
-    io.on("update_instrument", (data) => {
-      console.log(takenInstruments["piano"]);
-
-      roomState = data.roomState;
-      takenInstruments[data.instrument as keyof typeof takenInstruments] = true;
-      var btn = document.querySelector("#" + data.instrument);
-
-      btn?.classList.add("bg-darkbeige");
-
-      console.log(btn?.classList);
-
-      console.log(takenInstruments["piano"]);
-    });
-
-    io.on("game_started", (data) => {
-      roomState = data.roomState;
-    });
-
-    io.on("new_turn", (data) => {
-      roomState = data.roomState;
-    });
-
-    io.on("game_finished", (data) => {
-      roomState = data.roomState;
-    });
-    return () => io?.disconnect();
-  });
-
-  function copyID() {
-    var copyText = roomId !== null ? roomId : "None";
-    navigator.clipboard.writeText(copyText);
-  }
-</script>
-
 <div class="h-full bg-gradient-to-b from-purple to-pink">
   <div class="size-24 relative top-3 left-5">
     <img src={logo} alt="logo" />
@@ -323,3 +138,188 @@
     {/if}
   </div>
 </div>
+
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { io } from "$lib/socketConnection";
+    import logo from "$lib/images/garticband.png";
+    import playerIcon from "$lib/images/artist.png";
+    import { browser } from "$app/environment";
+  
+    // Import components
+    import UpNext from "$lib/components/UpNext.svelte";
+    import AlreadyWent from "$lib/components/AlreadyWent.svelte";
+    import WaitingForPlayers from "$lib/components/WaitingForPlayers.svelte";
+    import FinalComposition from "$lib/components/FinalComposition.svelte";
+  
+    // Types
+    import { type playerInRoom, type roomInfo } from "$lib/types";
+    import SelectInstrument from "$lib/components/SelectInstrument.svelte";
+  
+    // Name of user
+    let username = "";
+    if (browser) {
+      username = localStorage.getItem("username") || "No Name";
+    }
+  
+    const URLParams = $page.url.searchParams;
+    let roomId = $state<string | null>(null);
+    let roomState = $state<roomInfo>({
+      players: [],
+      selectPhase: false,
+      activeTurn: null,
+      isCompleted: false,
+    });
+  
+    let takenInstruments = $state({
+      drums: false,
+      piano: false,
+      synth: false,
+      bass: false,
+    });
+  
+    let genre = $state<string | null>("");
+  
+    const NUM_TIMESTEPS = 32;
+  
+    // Find ourself in the array
+    let me = $derived<playerInRoom | null>(
+      roomState.players.find((p) => p.id == io.id) ?? null
+    );
+  
+    let instrumentSelectActive = $derived<boolean>(roomState.selectPhase == true);
+    let instrumentDone = $derived<boolean>(
+      Object.values(takenInstruments).every((value) => value === true)
+    );
+  
+    let roomReadyToStart = $derived<boolean>(
+      !roomState.activeTurn && roomState.players.length == 4
+    );
+    let gameActive = $derived<boolean>(roomState.activeTurn != null);
+    let gameFinished = $derived<boolean>(roomState.isCompleted);
+  
+    let upNext = $derived<boolean | null>(
+      roomState.activeTurn && me?.turnNumber
+        ? roomState.activeTurn == me.turnNumber - 1
+        : null
+    );
+  
+    let activeTurn = $derived<boolean | null>(
+      roomState.activeTurn && me?.turnNumber
+        ? roomState.activeTurn == me.turnNumber
+        : null
+    );
+  
+    let alreadyWent = $derived<boolean | null>(
+      roomState.activeTurn && me?.turnNumber
+        ? roomState.activeTurn > me.turnNumber
+        : null
+    );
+  
+    const startSelect = () => {
+      io.emit("start_select", {});
+    };
+  
+    const chooseDrum = () => {
+      // add a check where it only does this if we don't have an instrument selected
+      if (
+        me?.sequencer.instrumentId == null &&
+        takenInstruments["drums"] == false
+      ) {
+        io.emit("choose_drum", {});
+      }
+    };
+    // add other instrument selections
+    const chooseSynth = () => {
+      // add a check where it only does this if we don't have an instrument selected
+      if (
+        me?.sequencer.instrumentId == null &&
+        takenInstruments["synth"] == false
+      ) {
+        io.emit("choose_synth", {});
+      }
+    };
+  
+    const chooseBass = () => {
+      // add a check where it only does this if we don't have an instrument selected
+      if (
+        me?.sequencer.instrumentId == null &&
+        takenInstruments["bass"] == false
+      ) {
+        io.emit("choose_bass", {});
+      }
+    };
+  
+    const choosePiano = () => {
+      // add a check where it only does this if we don't have an instrument selected
+      if (
+        me?.sequencer.instrumentId == null &&
+        takenInstruments["piano"] == false
+      ) {
+        io.emit("choose_piano", {});
+      }
+    };
+  
+    const startGame = () => {
+      io.emit("start_game", {});
+    };
+    $effect(() => {
+      if (!URLParams.get("id")) {
+        // Something went wrong...
+        return;
+      }
+      const id = URLParams.get("id");
+      io.emit("join_room", { roomId: id, name: username });
+  
+      // * * * Setup socket stuff
+      io.on("room_joined", (data) => {
+        roomId = data.roomId;
+        roomState = data.roomState;
+      });
+  
+      io.on("player_joined", (data) => {
+        roomState = data.roomState;
+      });
+  
+      io.on("player_left", (data) => {
+        roomState = data.roomState;
+      });
+  
+      io.on("select_started", (data) => {
+        roomState = data.roomState;
+        genre = data.genre;
+      });
+  
+      io.on("update_instrument", (data) => {
+        console.log(takenInstruments["piano"]);
+  
+        roomState = data.roomState;
+        takenInstruments[data.instrument as keyof typeof takenInstruments] = true;
+        var btn = document.querySelector("#" + data.instrument);
+  
+        btn?.classList.add("bg-darkbeige");
+  
+        console.log(btn?.classList);
+  
+        console.log(takenInstruments["piano"]);
+      });
+  
+      io.on("game_started", (data) => {
+        roomState = data.roomState;
+      });
+  
+      io.on("new_turn", (data) => {
+        roomState = data.roomState;
+      });
+  
+      io.on("game_finished", (data) => {
+        roomState = data.roomState;
+      });
+      return () => io?.disconnect();
+    });
+  
+    function copyID() {
+      var copyText = roomId !== null ? roomId : "None";
+      navigator.clipboard.writeText(copyText);
+    }
+  </script>
