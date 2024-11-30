2aea86c1c788a9b3d849d1e83e4070b2b5ef6f60
Parikshit Sood <parkysood@gmail.com>
2024-11-29 20:31:51 -0800

    finished game end screen

--- frontend/garticband/src/lib/components/GameOver.svelte
+++ frontend/garticband/src/lib/components/GameOver.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-  
-    import { type GameOverProps } from '../types'
-    const { io }: GameOverProps = $props()
+  import { type GameOverProps } from "../types";
+  const { io }: GameOverProps = $props();
 
-    const backLobby = () => {
-        io?.emit("back_lobby", {})
-    }
+  const backLobby = () => {
+    io?.emit("back_lobby", {});
+  };
 </script>
 
-<div>
-    <h1>You suck</h1>
-    <button onclick={backLobby}>Back to Lobby</button>
+<div class="flex flex-col flex-1 items-center justify-center">
+  <h1 class="text-6xl font-bold text-white mt-40">You suck</h1>
+  <button
+    class="bg-blue-500 text-black font-medium px-4 py-2 rounded-md mt-8 bg-beige shadow-lg shadow-black hover:shadow-lg hover:shadow-beige/30"
+    onclick={backLobby}>Back to Lobby</button
+  >
 </div>
