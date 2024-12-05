f3938f7096398dbaf41b90e351394bb3a1326d8e
Na Yoon Kang <nykang17@gmail.com>
2024-12-05 11:25:00 -0800

    Installing vitest
    We need to use vitest probably for svelte so added the depedency and
    changed vite.config accordingly

--- frontend/garticband/src/__tests__/hello.test.ts
+++ frontend/garticband/src/__tests__/hello.test.ts
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/svelte";
 import App from "../routes/+page.svelte";
+import { io } from "$lib/socketConnection";
 
 test("should render", () => {
   // const results = render(App, {props: {name: "world"}});
